595a473ae3fbd65bbb5693d53bc6c9ec5502be13
hoang1811-coder <vhoangssz1@gmail.com>
2024-11-22 01:10:06 +0700

    upload

---
+++ ss3.b3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define PI 3.14159
+int main() {
+    double r, chuVi, dienTich;
+    printf("Nhap ban kinh hinh tron (r): ");
+    scanf("%lf", &r);
+    chuVi = 2 * PI * r;
+    dienTich = PI * r * r;
+    printf("Chu vi hinh tron la: %.2f\n", chuVi);
+    printf("Dien tich hinh tron la: %.2f\n", dienTich);
+
+    return 0;
+}
