0d94b0fbb25ead85bd4b2adeba971ab745b99dfc
hoang1811-coder <vhoangssz1@gmail.com>
2024-11-22 01:09:35 +0700

    upload

---
+++ ss3.b1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string name;
+    cout << "Vui long nhap ten cua ban: ";
+    getline(cin, name);
+    cout << "Xin chao " << name << endl;
+
+    return 0;
+}
