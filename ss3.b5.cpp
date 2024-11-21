518df77cd4255d2147fe20edea1b51eef2b35899
hoang1811-coder <vhoangssz1@gmail.com>
2024-11-22 01:10:33 +0700

    upload

---
+++ ss3.b5.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    float canhDay, chieuCao, dienTich;
+    printf("Nhap do dai canh day cua tam giac: ");
+    scanf("%f", &canhDay);
+    printf("Nhap do cao cua tam giac: ");
+    scanf("%f", &chieuCao);
+    dienTich = 0.5 * canhDay * chieuCao;
+    printf("Dien tich cua tam giac la: %.2f\n", dienTich);
+
+    return 0;
+}
